3cf787dcd2c511d2adc01f9010c215b3299981a2
JJ Orozco López <jjorozcodev@gmail.com>
2021-06-19 08:52:01 -0600

    Using HotChocolate and configure endpoint
    Register the query engine and middleware.
    Define the GraphQL path.

--- GraphQLNetCore.Example/Startup.cs
+++ GraphQLNetCore.Example/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using HotChocolate;
+using HotChocolate.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,11 +13,16 @@ namespace GraphQLNetCore.Example
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Register Query engine and middleware for GraphQL
+            services.AddGraphQL(s => SchemaBuilder.New().AddServices(s).Create());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Define to use GraphQL
+            app.UseGraphQL("/graphql");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -23,7 +30,7 @@ namespace GraphQLNetCore.Example
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello JJ!");
             });
         }
     }
